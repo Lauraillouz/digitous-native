5a92834cc01568421df552f83d377d4f0a8c3f0c
Laura Illouz <laura.hortense@gmail.com>
2021-08-24 15:45:24 +0200

    when click on flag, capital shows (working only for 2 clicks

--- liste/src/components/List.js
+++ liste/src/components/List.js
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from "react";
 // React Native
-import { View, Text, FlatList, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 const List = () => {
   const [countries, setCountries] = useState([]);
+  const [click, setClick] = useState(false);
+  const [selected, setSelected] = useState("");
 
   const getCountries = () => {
     fetch("http://restcountries.eu/rest/v2/all")
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
         setCountries(res);
       });
   };
@@ -18,12 +26,24 @@ const List = () => {
     getCountries();
   }, []);
 
+  const handlePress = (item) => {
+    selected === item.numericCode || selected === ""
+      ? setClick((prevState) => !prevState)
+      : null;
+    setSelected(item.numericCode);
+    console.log(click, selected);
+  };
+
   const renderCountry = ({ item }) => {
     return (
       <View style={styles.list}>
-        <Image style={styles.flag} source={{ uri: item.flag }}></Image>
-        <Text>Country: {item.name}</Text>
-        <Text>Capital: {item.capital}</Text>
+        <Text>{item.name}</Text>
+        <TouchableOpacity onPress={() => handlePress(item)}>
+          <Image style={styles.flag} source={{ uri: item.flag }}></Image>
+        </TouchableOpacity>
+        {click && item.numericCode === selected ? (
+          <Text>Capital: {item.capital}</Text>
+        ) : null}
       </View>
     );
   };
