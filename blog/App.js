1093212d852db473eb1dbbc12ab0198cf9808b73
Laura Illouz <laura.hortense@gmail.com>
2021-08-26 10:54:10 +0200

    login component ok

--- blog/App.js
+++ blog/App.js
@@ -1,5 +1,5 @@
 import React, { createContext } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NativeRouter, Route } from "react-router-native";
 // Components
 import Login from "./src/views/Login";
@@ -10,20 +10,22 @@ const PostContext = createContext();
 
 const App = () => {
   return (
-    <PostContext.Provider>
-      <NativeRouter>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
-      </NativeRouter>
-      <Navbar />
-    </PostContext.Provider>
+    <View style={styles.container}>
+      <PostContext.Provider>
+        <NativeRouter>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+        </NativeRouter>
+        <Navbar />
+      </PostContext.Provider>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#1D1D1D",
     alignItems: "center",
     justifyContent: "center",
   },
--- blog/src/views/Login.js
+++ blog/src/views/Login.js
@@ -1,12 +1,81 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { useHistory } from "react-router-native";
 
 const Login = () => {
+  const [isIDValid, setIsIDValid] = useState(false);
+
+  const history = useHistory();
+
+  const checkID = (val) => {
+    if (val.length < 10 && val.length > 1) {
+      setIsIDValid(true);
+    } else {
+      setIsIDValid(false);
+    }
+  };
+
+  const handlePress = () => {
+    if (isIDValid) {
+      history.push("/home");
+    } else {
+      Alert.alert(
+        "Your ID needs to contain between between 1 and 10 characters"
+      );
+    }
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome</Text>
+      <TextInput style={styles.input} onChangeText={checkID} />
+      <TouchableOpacity style={styles.btn} onPress={handlePress}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.8,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 32,
+    color: "white",
+    textAlign: "center",
+  },
+  btn: {
+    padding: 12,
+    backgroundColor: "white",
+    borderRadius: 15,
+    marginRight: 100,
+    marginLeft: 100,
+  },
+  btnText: {
+    textAlign: "center",
+    color: "#1A1A18",
+  },
+  input: {
+    margin: 16,
+    width: 300,
+    borderWidth: 1,
+    padding: 12,
+    borderRadius: 15,
+    borderColor: "white",
+    color: "white",
+  },
+});
 
 export default Login;
