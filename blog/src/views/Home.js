0316ae9cb9fa037d7e371061a8ab4d8688115801
Laura Illouz <laura.hortense@gmail.com>
2021-08-26 16:19:44 +0200

    added Posts component

--- blog/src/views/Home.js
+++ blog/src/views/Home.js
@@ -1,59 +1,24 @@
-import React, { useContext, useEffect } from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
-// Context
-import { PostContext } from "../../App";
-import { UserContext } from "../../App";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+// Component
+import Posts from "../components/Posts";
 
 const Home = () => {
-  const { posts, setPosts } = useContext(PostContext);
-  const { ID, setID } = useContext(UserContext);
-
-  const getPosts = () => {
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
-      .then((res) => res.json())
-      .then((res) => {
-        setPosts(res);
-      });
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  const renderPosts = ({ item }) => {
-    if (item.userId === parseInt(ID)) {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.title}>{item.title}</Text>
-          <Text style={styles.post}>{item.body}</Text>
-          <Text style={styles.text}>{item.userId}</Text>
-        </View>
-      );
-    }
-  };
-
   return (
-    <FlatList
-      data={posts}
-      renderItem={renderPosts}
-      keyExtractor={(item, index) => index.toString()}
-    />
+    <View>
+      <Text style={styles.title}>Your Timeline</Text>
+      <Posts />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 32,
-    backgroundColor: "white",
-    padding: 24,
-    borderRadius: 20,
-  },
-
   title: {
-    color: "blue",
+    marginTop: 40,
+    textAlign: "center",
+    fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 12,
+    color: "white",
   },
 });
 
